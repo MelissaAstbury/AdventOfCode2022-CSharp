bcaf80c1e12e0c110d3836c4a3b037eadc92fe06
Melissa Astbury <melissa.astbury@footasylum.com>
2022-12-03 18:31:34 +0000

    Day one of advent of code - read from file and count the top calories

---
+++ AdventOfCode2022/Day1/CalorieCounting.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode2022.Day1
+{
+    public class CalorieCounting
+    {
+        public int CalculateMaxCalorie(string text)
+        {
+            IDictionary<int, int> groupTotals = new Dictionary<int, int>();
+            var key = 0;
+            string[] listOfNums = text.Split("\n");
+
+            foreach (var item in listOfNums)
+            {
+                string cleanItem = item.Replace("\n", "");
+                if (cleanItem != "")
+                {
+                    if (groupTotals.ContainsKey(key))
+                    {
+                        groupTotals[key] += int.Parse(item);
+                    }
+                    else
+                    {
+                        groupTotals.Add(key, int.Parse(item));
+                    }
+                }
+                else
+                {
+                    key++;
+                }
+            }
+
+            var maxValue = groupTotals.Values.Max();
+
+            foreach(KeyValuePair<int, int> group in groupTotals.OrderBy(key => key.Value))
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", group.Key, group.Value);
+            };
+
+            return maxValue;
+
+
+
+
+            // 1st ATTEMPT
+
+            //var answer = 0;
+            //string[] groupedNums = text.Split("\n\n");
+
+            //foreach (var group in groupedNums)
+            //{
+            //    string cleanItem = group.Replace("\n", "");
+
+            //    //Get totals of the group
+            //    int sumOfNums = cleanedGroup.Sum(x => Convert.ToInt32(x));
+            //    if (answer < sumOfNums)
+            //    {
+            //        answer = sumOfNums;
+            //    }
+
+            //}
+
+            //return 0;
+        }
+    }
+};
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2022.Day1;
+
+namespace AdventOfCode2022
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"calories.txt");
+
+            var day1 = new CalorieCounting();
+            Console.WriteLine($"Day 1 - {day1.CalculateMaxCalorie(text)}");
+        }
+    }
+}
