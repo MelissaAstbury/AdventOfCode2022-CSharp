a16244d4079b2a2fd72ca3f30c8993c4508e2530
Melissa Astbury <melissa.astbury@footasylum.com>
2022-12-06 20:40:54 +0000

    part two of day six complete. Commented out code added to show part one and part two in the same code

--- AdventOfCode2022/Day6/Characters.cs
+++ AdventOfCode2022/Day6/Characters.cs
@@ -16,12 +16,14 @@ namespace AdventOfCode2022.Day6
 
                 groupedText.Add(charToString);
 
-                if(groupedText.Count == 5)
+                //part one code should be if(groupedText.Count == 5)
+                if (groupedText.Count == 15)
                 {
                     groupedText.Remove(groupedText[0]);
 
                     var nonDuplcates = groupedText.GroupBy(x => x).Where(j => j.Count() == 1).Select(j => j.Key).ToList();
-                    if (nonDuplcates.Count == 4)
+                    //part one code should be if (nonDuplcates.Count == 4)
+                    if (nonDuplcates.Count == 14)
                     {
                         Console.WriteLine(nonDuplcates);
                         return result = i + 1;
@@ -31,6 +33,11 @@ namespace AdventOfCode2022.Day6
                 return result;
         }
 
-        //4092
+        // PART ONE
+        //4092 - attempt one - WRONG
+        //1987 -attempt two - CORRECT
+
+        //PART TWO
+        //3059 - attempt one - CORRECT
     }
 };
